939079bd4acd5619075e3cc3e0454ca6287458a1
Alexey Shary <aliaksei.shary@oracle.com>
2022-02-10 21:13:08 -0800

    Maximum Subarray from LeetCode

---
+++ src/com/leetcode/MaximumSubarray.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+/**
+ * 53. Maximum Subarray
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int max = nums[0];
+        int sum = max;
+        for (int i = 1; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
---
+++ test/com/leetcode/MaximumSubarrayTest.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumSubarrayTest {
+    @Test
+    void testMaxSubArray() {
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        assertEquals(maximumSubarray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}), 6); // [4,-1,2,1] has the largest sum = 6
+        assertEquals(maximumSubarray.maxSubArray(new int[]{ 1 }), 1);
+        assertEquals(maximumSubarray.maxSubArray(new int[]{ 5, 4, -1, 7, 8 }), 23);
+    }
+}
