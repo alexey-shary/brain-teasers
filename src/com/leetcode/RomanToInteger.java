ad2b4142ad43c24b895c641f60cde01a9e5488ce
Alexey Shary <aliaksei.shary@oracle.com>
2022-05-03 12:41:51 -0700

    LeetCode: Roman to Integer problem solved

---
+++ src/com/leetcode/RomanToInteger.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * 13. Roman to Integer
+ *
+ * Given a roman numeral, convert it to an integer.
+ *
+ */
+public class RomanToInteger {
+    private static HashMap<String, Integer> translations = new HashMap<>();
+
+    static {
+        translations.put("I", 1);
+        translations.put("V", 5);
+        translations.put("X", 10);
+        translations.put("L", 50);
+        translations.put("C", 100);
+        translations.put("D", 500);
+        translations.put("M", 1000);
+
+        translations.put("IV", 4);
+        translations.put("IX", 9);
+        translations.put("XL", 40);
+        translations.put("XC", 90);
+        translations.put("CD", 400);
+        translations.put("CM", 900);
+    }
+
+    public int romanToInt(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String c = String.valueOf(s.charAt(i));
+            if (i != s.length() - 1 && translations.get(s.substring(i, i + 2)) != null) {
+                result += translations.get(s.substring(i, i + 2));
+                i++;
+            } else {
+                result += translations.get(c);
+            }
+        }
+        return result;
+    }
+}
---
+++ test/com/leetcode/RomanToIntegerTest.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanToIntegerTest {
+    @Test
+    public void testRomanToInt() {
+        RomanToInteger rti = new RomanToInteger();
+        assertEquals(3, rti.romanToInt("III"));
+        assertEquals(58, rti.romanToInt("LVIII"));
+        assertEquals(1994, rti.romanToInt("MCMXCIV"));
+    }
+}
