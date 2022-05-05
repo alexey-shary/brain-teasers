22e83affdcef6cbbe9073210dbe0cec42dd09680
Alexey Shary <aliaksei.shary@oracle.com>
2022-05-05 16:47:19 -0700

    LeetCode: improved Merge Two Sorted Lists solution

--- src/com/leetcode/easy/MergeTwoSortedLists.java
+++ src/com/leetcode/easy/MergeTwoSortedLists.java
@@ -14,13 +14,7 @@ public class MergeTwoSortedLists {
         if (list1 == null || list2 == null) return list1 == null ? list2 : list1;
 
         ListNode start = new ListNode();
-        ListNode current = list1.val <= list2.val ? list1 : list2;
-        start.next = current;
-        if (list1.val <= list2.val) {
-            list1 = list1.next;
-        } else {
-            list2 = list2.next;
-        }
+        ListNode current = start;
 
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
