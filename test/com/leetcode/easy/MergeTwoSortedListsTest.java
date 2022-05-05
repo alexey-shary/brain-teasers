738ea234fa4348c3f11c2922f34f73f76e47ff44
Alexey Shary <aliaksei.shary@oracle.com>
2022-05-05 16:43:14 -0700

    LeetCode: Merge Two Sorted Lists problem solved

---
+++ test/com/leetcode/easy/MergeTwoSortedListsTest.java
@@ -0,0 +1,10 @@
+package com.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+public class MergeTwoSortedListsTest {
+    @Test
+    public void testMergeTwoLists() {
+        // TODO
+    }
+}
