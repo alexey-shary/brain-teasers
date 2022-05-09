82a27f551ce75e125a4beadb6a79dde3df5a1bf0
Alexey Shary <aliaksei.shary@oracle.com>
2022-05-09 14:53:16 -0700

    XYZ: added iterative solution by @minskv and benchmarking with JMH

--- src/xyz/my/BasketballTeams.java
+++ src/xyz/my/BasketballTeams.java
@@ -1,11 +1,26 @@
 package xyz.my;
 
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+
 public class BasketballTeams {
-    public Set<TeamPair> allTeams(String[] players) {
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchmarkAllTeamsRecursive(BenchmarkState benchmarkState) {
+        allTeamsRecursive(benchmarkState.players);
+    }
+
+    /**
+     * Solved by me.
+     */
+    public Set<TeamPair> allTeamsRecursive(String[] players) {
         Set<TeamPair> pairs = new HashSet<>();
         selectTeam(pairs, players, new HashSet<>(), new HashSet<>(), 0);
         return pairs;
@@ -25,6 +40,72 @@ public class BasketballTeams {
             second.add(players[index]);
             selectTeam(pairs, players, first, second, index + 1);
             second.remove(players[index]);
+        }
+    }
+
+    /* ############################################################################################################# */
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchmarkAllTeamsIterative(BenchmarkState benchmarkState) {
+        allTeamsIterative(benchmarkState.players);
+    }
+
+    /**
+     * Solved by Vladimir.
+     */
+    public Set<TeamPair> allTeamsIterative(String[] players) {
+        Set<TeamPair> result = new HashSet<>();
+        final int N = players.length - 1;
+        int[] teamAIndexes = new int[players.length / 2];
+        int[] teams = new int[players.length];
+        for (int i = 0; i < teamAIndexes.length; i++)
+            teamAIndexes[i] = i;
+        int offset, index, value;
+        int limit_for_first_A_player_index = players.length % 2 == 0 ? 1 : N / 2 + 2;
+        while (teamAIndexes[0] < limit_for_first_A_player_index) {
+            Set<String> teamAPlayers = new HashSet<>();
+            Set<String> teamBPlayers = new HashSet<>();
+            Arrays.fill(teams, 0);
+            for (int i = 0; i < teamAIndexes.length; i++)
+                teams[teamAIndexes[i]] = 1;
+            for (int i = 0; i < teams.length; i++)
+                if (teams[i] == 1)
+                    teamAPlayers.add(players[i]);
+                else
+                    teamBPlayers.add(players[i]);
+            result.add(new TeamPair(teamAPlayers, teamBPlayers));
+
+            if (teamAIndexes[teamAIndexes.length - 1] < N) {
+                teamAIndexes[teamAIndexes.length - 1]++;
+                continue;
+            }
+            offset = 0;
+            while (teamAIndexes[teamAIndexes.length - 1 - offset] == N - offset && teamAIndexes.length - 1 - offset > 0)
+                offset++;
+            index = teamAIndexes.length - 1 - offset;
+            teamAIndexes[index]++;
+            value = teamAIndexes[index] + 1;
+            for (int i = index + 1; i < teamAIndexes.length; i++) {
+                teamAIndexes[i] = value++;
+            }
+        }
+        return result;
+    }
+
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+        String[] players;
+
+        @Setup(Level.Iteration)
+        public void setup() {
+            players = new String[]{ "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10" };
+        }
+
+        @TearDown(Level.Iteration)
+        public void tearDown() {
+            players = null;
         }
     }
 }
--- test/xyz/my/BasketballTeamsTest.java
+++ test/xyz/my/BasketballTeamsTest.java
@@ -1,5 +1,6 @@
 package xyz.my;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -7,17 +8,30 @@ import java.util.Set;
 
 public class BasketballTeamsTest {
     @Test
-    public void testAllPairs() {
+    //@Disabled
+    public void testAllTeamsRecursive() {
         long start = Instant.now().toEpochMilli();
         BasketballTeams bt = new BasketballTeams();
-        String[] players = new String[]{ "Alexey", "Олександр", "Andriy", "Misha", "Vasyl", "Svetlana", "Bogdan", "Victoria", "Aleksei", "Pavel"};
-        // for (int i = 0; i < 10000; i++) {
-            Set<TeamPair> result = bt.allTeams(players);
-        // }
-        for (TeamPair tp : result) {
-            System.out.println(tp);
-        }
-        System.out.println("\nTotal number of all possible games with every distinct team: " + result.size() + " (for " + players.length + " players)");
-        System.out.println(Instant.now().toEpochMilli() - start + " milliseconds");
+        String[] players = new String[]{ "Alexey", "Олександр", "Andriy", "Misha", "Vasyl", "Svetlana", "Bogdan", "Victoria", "Aleksei", "Pavel" };
+        Set<TeamPair> result = bt.allTeamsRecursive(players);
+        //for (TeamPair tp : result) {
+            //System.out.println(tp);
+        //}
+        System.out.println("Total number of all possible games with every distinct team: " + result.size() + " (for " + players.length + " players)");
+        System.out.println("testAllTeamsRecursive: " + (Instant.now().toEpochMilli() - start) + " milliseconds");
+    }
+
+    @Test
+    //@Disabled
+    public void testAllTeamsIterative() {
+        long start = Instant.now().toEpochMilli();
+        BasketballTeams bt = new BasketballTeams();
+        String[] players = new String[]{ "Alexey", "Олександр", "Andriy", "Misha", "Vasyl", "Svetlana", "Bogdan", "Victoria", "Aleksei", "Pavel" };
+        Set<TeamPair> result = bt.allTeamsIterative(players);
+        //for (TeamPair tp : result) {
+            //System.out.println(tp);
+        //}
+        System.out.println("Total number of all possible games with every distinct team: " + result.size() + " (for " + players.length + " players)");
+        System.out.println("testAllTeamsIterative: " + (Instant.now().toEpochMilli() - start) + " milliseconds");
     }
 }
