5fc9a61067c6540a6926ceb8c733feabb30c01cc
Alexey Shary <aliaksei.shary@oracle.com>
2022-05-26 11:40:26 -0700

    LeetCode: Rotate Image problem solved

---
+++ src/com/leetcode/medium/RotateImage.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium;
+
+/**
+ * 48. Rotate Image
+ *
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ */
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 1) return;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length - i - 1; j++) {
+                int top = matrix[i][j];
+                int right = matrix[j][matrix.length - i - 1];
+                int bottom = matrix[matrix.length - i - 1][matrix.length - j - 1];
+                int left = matrix[matrix.length - j - 1][i];
+
+                matrix[i][j] = left;
+                matrix[j][matrix.length - i - 1] = top;
+                matrix[matrix.length - i - 1][matrix.length - j - 1] = right;
+                matrix[matrix.length - j - 1][i] = bottom;
+            }
+        }
+    }
+}
---
+++ test/com/leetcode/medium/RotateImageTest.java
@@ -0,0 +1,11 @@
+package com.leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+public class RotateImageTest {
+    @Test
+    public void testRotate() {
+        RotateImage ri = new RotateImage();
+        ri.rotate(new int[][] {{1,2,3}, {4,5,6}, {7,8,9}});
+    }
+}
